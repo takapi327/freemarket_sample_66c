c7314dc83bbf1b363c030e2763901cd0c0cfa8ac
akimegane0730 <akimegane0730o@gmail.com>
2020-01-09 16:38:55 +0900

    add image if

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,21 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'freemarket66c'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws_access_key_id,
+      aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 
-  config.fog_directory  = 'freemarket66c'
+  
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket66c'
 end
