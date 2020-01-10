46eb50a58b13029b87b3e44abc2fdf13d36eeec6
SIIC8848 <gx9901dx.seiichiro@gmail.com>
2020-01-10 16:23:50 +0900

    Add validation to UserModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #deviseのデフォルトバリデーションはemail,passwordのみのため、追加しています
+  validates :nickname, presence: true, length: { maximum: 6 }
 end
