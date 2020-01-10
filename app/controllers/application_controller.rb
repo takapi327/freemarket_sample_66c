03eab7fc87634c0c918e9846b617243be44d434e
SIIC8848 <gx9901dx.seiichiro@gmail.com>
2020-01-10 16:21:58 +0900

    Add security measures to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # 悪意のあるユーザーの入力に対してのセキュリティ対策
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # サインアップ時に入力された「nickname」キーの内容の保存を許可しています
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
