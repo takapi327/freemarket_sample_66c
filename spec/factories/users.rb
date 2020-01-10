8acda457db55b9c5d646e9dd3140511465841543
SIIC8848 <gx9901dx.seiichiro@gmail.com>
2020-01-10 18:01:56 +0900

    Test code for signup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
+
   factory :user do
-    
+    nickname              {"takao"}
+    email                 {"takao@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
   end
+
 end
