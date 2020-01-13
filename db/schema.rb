b83d5e0d47dad7aefeb06b8398c21933c5cd9879
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-01-13 16:54:49 +0900

    product-model-create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_115307) do
+ActiveRecord::Schema.define(version: 2020_01_13_073004) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "derivery", null: false
+    t.string "name", null: false
+    t.integer "status", default: 0, null: false
+    t.integer "price", null: false
+    t.integer "burden", default: 0, null: false
+    t.text "content", null: false
+    t.date "day", null: false
+    t.bigint "buyer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["buyer_id"], name: "index_products_on_buyer_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +41,6 @@ ActiveRecord::Schema.define(version: 2020_01_09_115307) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
+  add_foreign_key "products", "users", column: "buyer_id"
 end
