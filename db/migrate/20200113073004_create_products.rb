59d1399479f26f2deac5fbc6e63e46cada2eb219
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-01-13 17:08:12 +0900

    products-create-controller

--- db/migrate/20200113073004_create_products.rb
+++ db/migrate/20200113073004_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
 
-      t.references  :user,   null:false, foreign_key:true                    #ユーザーID
+      t.references :user,   null:false, foreign_key:true                     #ユーザーID
       t.string     :derivery,  null:false                                    #配達日
       t.string     :name,      null:false                                    #商品名
       t.integer    :status,    null:false, default: 0                        #商品の状態
@@ -10,7 +10,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer    :burden,    null:false, default: 0                        #配送料
       t.text       :content,   null:false                                    #商品の詳細
       t.date       :day,       null:false                                    #配達日数
-      t.references  :buyer,  foreign_key:{to_table: :users}                #購入者情報
+      t.references :buyer,  foreign_key:{to_table: :users}                   #購入者情報
 
       t.timestamps
     end
