cbb1e67ace671a8eb558b4487f4ba8f99f2b493e
takfujii29 <takfujii29@icloud.com>
2020-01-15 16:49:58 +0900

    add_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @products = Product.all
+    @product = Product.new
+    @user = User.find(params[:id])
   end
 
   def edit
+    @products = Product.all
+    @product = Product.new
+    @users = User.all
+    @user = User.new
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
   resources :products, only: [:index, :new, :show]
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit]
 end
