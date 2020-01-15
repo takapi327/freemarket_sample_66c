6e44165b930acf0658055f958928c32a6e23249f
akimegane0730 <akimegane0730o@gmail.com>
2020-01-15 10:39:42 +0900

    set username and password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+  # basic認証のための記述
+  before_action :basic_auth
   # 悪意のあるユーザーの入力に対してのセキュリティ対策
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  
+  def basic_auth
+    #ユーザー名とパスワードでBasic認証ができるように設定しています。
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '66_mercari_c' && password == '66c'
+    end
+  end
 
   protected
 
