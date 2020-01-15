4950346ac868ef10853191c59aa51d3ad16f4e94
akimegane0730 <akimegane0730o@gmail.com>
2020-01-15 10:50:55 +0900

    run basic only production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   # basic認証のための記述
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   # 悪意のあるユーザーの入力に対してのセキュリティ対策
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  # 本番環境でのみ実行するための記述
+  def production?
+    Rails.env.production?
+  end
   
   def basic_auth
     # ターミナルで設定した環境変数を使ってログインできるようにした。
