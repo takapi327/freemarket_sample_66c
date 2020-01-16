ec57ab1ecc9467b6e3c96ec44af3bbc6f9e3e14d
miyat-yuka <58365099+miyat-yuka@users.noreply.github.com>
2020-01-16 15:35:29 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "products#buy"
+  root "products#index"
   resources :products, only: [:index, :new, :show]
   resources :users, only: [:index, :show, :edit]
 end
