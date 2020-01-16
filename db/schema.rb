d5f0ffba8324442d7f4b38017a3f2cd1905753e5
SIIC8848 <gx9901dx.seiichiro@gmail.com>
2020-01-16 20:10:14 +0900

    Create sns_credentials model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_073004) do
+ActiveRecord::Schema.define(version: 2020_01_14_092311) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_01_13_073004) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -43,4 +52,5 @@ ActiveRecord::Schema.define(version: 2020_01_13_073004) do
 
   add_foreign_key "products", "users"
   add_foreign_key "products", "users", column: "buyer_id"
+  add_foreign_key "sns_credentials", "users"
 end
