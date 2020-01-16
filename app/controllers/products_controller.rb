86714b06f5d0ac8c647a1ab475f13fa188867eae
akimegane0730 <akimegane0730o@gmail.com>
2020-01-16 17:08:31 +0900

    インスタンス　追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.includes(:user).order('created_at DESC').limit(10)
+    @product = Product.new
   end
 
   def show
