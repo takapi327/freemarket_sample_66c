162146b4b3a486ae1ed24f64165ddab0ac5211c8
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-01-16 19:21:07 +0900

    change-file
    削除し忘れを削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,6 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.user_id = current_user.id
     @product.save
     redirect_to new_user_session_path
   end
