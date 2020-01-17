82939762dc2ef1b8f9cd09b0b6874ab5cb961fdb
takfujii29 <takfujii29@icloud.com>
2020-01-17 15:01:26 +0900

    css-update

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
-  get 'users/logout' => 'users#logout'
-  get 'users/cardregister' => 'users#cardregister'
+  get 'users/logout', to: 'users#logout'
+  get 'users/cardregister', to: 'users#cardregister'
   resources :products, only: [:index, :new, :create, :show]
   resources :users, only: [:index, :show, :edit]
 end
