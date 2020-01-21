48b740f4239451a24a61dd8976e5f6613f8f6887
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-01-21 16:34:19 +0900

    add_Drag-and-Drop
    画像投稿をドラッグアンドドロップでできるようにした。

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -6,14 +6,14 @@ $(function(){
       var profit = document.getElementById('id').value;
       var tax_price = function(price){
         var tax = 0.1;
-        return price - price * tax;
+        return Math.round(price - price * tax);
       }
 
       var tax = function(price){
         var tax = 0.1;
-        return price * tax;
+        return Math.round(price * tax);
       }
-      if(profit >> 5){
+      if(profit >> 6 || 8 >> profit){
         document.getElementById('tax').textContent = tax(profit);
         document.getElementById('profit').textContent = '¥' + tax_price(profit);
       }
