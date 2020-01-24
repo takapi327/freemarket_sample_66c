832d52cee3ef21d361c319b8179042270fd652db
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-01-24 18:36:36 +0900

    Add_search_product_link
    検索した商品から詳細ページへのリンクを設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get 'users/cardregister', to: 'users#cardregister'
   resources :products
   resources :users, only: [:index, :show, :edit]
-  resources :searches, only: [:index]
+  resources :searches, only: [:index, :show]
 end
