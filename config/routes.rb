5c7ee6fabb8a537104c7ebd1fc34d65a3e369f58
SIIC8848 <gx9901dx.seiichiro@gmail.com>
2020-01-27 13:50:41 +0900

    Route setting for wizard-style
    ログイン機能にウィザード形式を実装するために呼び出し先ファイル用のルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,16 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'user_details', to: 'users/registrations#new_cellphone'
+    post 'user_details', to: 'users/registrations#create_tel'
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+    get 'complete_registrations', to: 'users/registrations#complete_registration'
+  end
   root "products#index"
   get 'users/logout' => 'users#logout'
   get 'users/cardregister' => 'users#cardregister'
   resources :products, only: [:index, :new, :create, :show]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :new]
 end
