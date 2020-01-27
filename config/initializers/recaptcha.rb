265ac52d843f7625fdde43279301db4687cac054
SIIC8848 <gx9901dx.seiichiro@gmail.com>
2020-01-27 13:48:54 +0900

    Recaptcha environment variable setting
    reCAPTCHAを利用するための環境変数をアプリで読み込むための設定

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,5 @@
 #reCAPTCHAを利用するための環境変数をアプリで読み込むための記述
-Recaptcha.configure do |config|   
-  config.site_key  = ENV['RECAPTCHA_SITE_KEY']
-  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']   
-# Uncomment the following line if you are using a proxy server:   
-# config.proxy = 'http://myproxy.com.au:8080' 
+Recaptcha.configure do |config|
+  config.site_key = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
